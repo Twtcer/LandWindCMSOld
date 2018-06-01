15896fb71b472e31a0213bb7b29ea92ed2191320
maidouofgithub <517773735@qq.com>
2018-06-01 17:33:46 +0800

    添加项目文件。

---
+++ LandWindCMS.Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LandWindCMS.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region  EF Test   
+            using (var cnx = new Model.LandWindCMSEntities())
+            { 
+                //read 
+                var userList = cnx.User.OrderByDescending(a=>a.Id).Take(3);
+                foreach (var user in userList)
+                {
+                    Console.WriteLine($"userId{user.Id}{user.UserName}{user.RealName}");
+                }
+            }
+
+            #endregion
+        }
+    }
+}
